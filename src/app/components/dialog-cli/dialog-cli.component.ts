9a082d28452b5b11f7f9914a4398c0a6258bd84f
Steve ZARETTI <kossolax@gmail.com>
2022-08-02 16:50:49 +0200

    working console submenu

--- src/app/components/dialog-cli/dialog-cli.component.ts
+++ src/app/components/dialog-cli/dialog-cli.component.ts
@@ -4,51 +4,95 @@ import { IPAddress, NetworkAddress } from 'src/app/models/address.model';
 import { RouterHost, SwitchHost } from 'src/app/models/node.model';
 
 abstract class TerminalCommand {
+  parent: TerminalCommand;
   name: string;
+  prompt: string;
 
   subCommands: { [key: string]: TerminalCommand } = {};
 
-  constructor(name: string) {
+  constructor(name: string, prompt: string = "") {
     this.name = name;
+    this.prompt = prompt;
+    this.parent = this;
   }
 
   public registerCommand(command: TerminalCommand) {
     this.subCommands[command.name] = command;
   }
 
-  public exec(node: SwitchHost|RouterHost, command: string, args: string[]) {
+  public exec(node: SwitchHost|RouterHost, command: string, args: string[]): TerminalCommand|null {
+    if( command === 'end' || command === 'exit' ) {
+      console.log('Exiting...', this.parent);
+      return this.parent;
+    }
+
+    if( command == this.name )
+      return this;
+
     if (command in this.subCommands) {
-      this.subCommands[command].exec(node, command, args);
+      return this.subCommands[command].exec(node, command, args);
     }
-    else {
-      throw new Error(`Command "${command}" not found.`);
-    }
+
+    throw new Error(`Command ${command} not found.`);
   }
 
 }
 class PingCommand extends TerminalCommand {
-  constructor() {
+  constructor(parent: TerminalCommand) {
     super('ping');
+    this.parent = parent;
   }
 
-  override exec(node: SwitchHost|RouterHost, command: string, args: string[]) {
+  override exec(node: SwitchHost|RouterHost, command: string, args: string[]): TerminalCommand|null {
     if( args.length < 1 )
       throw new Error('ping requires a hostname');
 
     node.send("ping", new IPAddress(args[0]));
+    return null;
   }
 }
 class AdminCommand extends TerminalCommand {
-  constructor() {
-    super('enable');
+  constructor(parent: TerminalCommand) {
+    super('enable', '#');
+    this.parent = parent;
+  }
+
+  override exec(node: SwitchHost|RouterHost, command: string, args: string[]): TerminalCommand|null {
+    let location = super.exec(node, command, args);
+
+    if( command === this.name )
+      console.log(`${node.name} is now in admin mode.`);
+
+    return location;
   }
 }
 class Terminal extends TerminalCommand {
-  constructor() {
-    super('root');
+  private location: TerminalCommand;
 
-    this.registerCommand(new AdminCommand());
-    this.registerCommand(new PingCommand());
+  constructor() {
+    super('root', '$');
+    this.location = this;
+
+    this.registerCommand(new AdminCommand(this));
+    this.registerCommand(new PingCommand(this));
+  }
+
+  public override exec(node: SwitchHost | RouterHost, command: string, args: string[]): TerminalCommand|null {
+    let location;
+
+    if( this.location === this )
+      location = super.exec(node, command, args);
+    else
+      location = this.location.exec(node, command, args);
+
+    if( location )
+      this.location = location;
+
+    return this;
+  }
+
+  public get Prompt(): string {
+    return `${this.location.prompt}`;
   }
 }
 
@@ -68,9 +112,8 @@ export class DialogCliComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngAfterViewInit(){
-
-    this.child.write(FunctionsUsingCSI.cursorColumn(1) + '$ ');
+  ngAfterViewInit() {
+    this.child.write(`\n ${FunctionsUsingCSI.cursorColumn(1)} ${this.terminal.Prompt} `);
 
     //...
     this.child.keyEventInput.subscribe(e => {
@@ -87,12 +130,12 @@ export class DialogCliComponent implements AfterViewInit {
             this.terminal.exec(this.node as SwitchHost|RouterHost, command[0], command.slice(1));
           }
           catch( e ) {
-            this.child.write(`\r\n ${e} \r\n`);
+            this.child.write(`\n ${FunctionsUsingCSI.cursorColumn(1)} ${e} `);
           }
         }
 
 
-        this.child.write('\n' + FunctionsUsingCSI.cursorColumn(1) + '$ '); // \r\n
+        this.child.write(`\n ${FunctionsUsingCSI.cursorColumn(1)} ${this.terminal.Prompt} `);
       }
       else if( key == 'Backspace' ) {
         if (this.child.underlying.buffer.active.cursorX > 2) {
