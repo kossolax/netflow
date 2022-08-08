a853c5d28460d1d72940db5c73d90b97693e0bf7
Steve ZARETTI <kossolax@gmail.com>
2022-08-08 19:12:24 +0200

    fix karma timeout

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
+    browserNoActivityTimeout: 60000,
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
