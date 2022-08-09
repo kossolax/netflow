a2f28464c23e9451ea4ca98536bd7054c1bb241a
Steve ZARETTI <kossolax@gmail.com>
2022-08-09 07:56:32 +0200

    ethernet test

---
+++ src/app/models/protocols/ethernet.spec.ts
@@ -0,0 +1,96 @@
+import { catchError, take, timeout } from "rxjs";
+import { SchedulerService, SchedulerState } from "src/app/services/scheduler.service";
+import { EthernetInterface } from "../layers/datalink.model";
+import { Link } from "../layers/physical.model";
+import { DatalinkMessage } from "../message.model";
+import { SwitchHost } from "../node.model";
+import { SimpleListener } from "./protocols.model";
+
+describe('Ethernet protocol', () => {
+  let A: SwitchHost;
+  let B: SwitchHost;
+  let C: SwitchHost;
+  let AB: Link;
+  let BC: Link;
+
+  let listener: SimpleListener;
+
+  beforeEach(async () => {
+    A = new SwitchHost();
+    A.name = "A";
+    A.addInterface().up();
+
+
+    B = new SwitchHost();
+    B.name = "B";
+    B.addInterface().up();
+    B.addInterface().up();
+
+    C = new SwitchHost();
+    C.name = "B";
+    C.addInterface().up();
+
+    AB = new Link(A.getInterface(0), B.getInterface(0));
+    BC = new Link(B.getInterface(1), C.getInterface(0));
+
+    listener = new SimpleListener();
+
+    SchedulerService.Instance.Speed = SchedulerState.FASTER;
+  });
+
+  it('Switch[0]->ETH-->[0]Switch[0]-->[0]Switch', (done) => {
+    C.getInterface(0).addListener(listener);
+
+    const message = `Hello World! ${Math.random()}`;
+    const trame = new DatalinkMessage(message, A.getInterface(0).getMacAddress(), C.getInterface(0).getMacAddress());
+
+    A.send(trame);
+
+    listener.receiveTrame$.pipe(
+      take(1),
+    ).subscribe(packet => {
+      expect(packet.payload).toBe(message);
+      done();
+    });
+  });
+
+  it('Switch[0]->ETH-->[1]Switch[1]-->[1]Switch', (done) => {
+    C.getInterface(0).addListener(listener);
+
+    const message = `Hello World! ${Math.random()}`;
+    const trame = new DatalinkMessage(message, A.getInterface(0).getMacAddress(), C.getInterface(0).getMacAddress());
+
+    (B.getInterface(0) as EthernetInterface).addVlan(1);
+    (B.getInterface(1) as EthernetInterface).addVlan(1);
+    (C.getInterface(0) as EthernetInterface).addVlan(1);
+
+    A.send(trame);
+    listener.receiveTrame$.pipe(
+      take(1),
+      timeout(1000),
+      catchError( async () => { })
+    ).subscribe(packet => {
+      expect(packet).toBeUndefined();
+      done();
+    });
+  });
+
+  it('Switch[0]->ETH-->[0]Switch[1]-->[1]Switch', (done) => {
+    C.getInterface(0).addListener(listener);
+
+    const message = `Hello World! ${Math.random()}`;
+    const trame = new DatalinkMessage(message, A.getInterface(0).getMacAddress(), C.getInterface(0).getMacAddress());
+
+    (B.getInterface(1) as EthernetInterface).addVlan(1);
+    (C.getInterface(0) as EthernetInterface).addVlan(1);
+
+    A.send(trame);
+    listener.receiveTrame$.pipe(
+      take(1),
+    ).subscribe(packet => {
+      expect(packet.payload).toBe(message);
+      done();
+    });
+  });
+
+});
