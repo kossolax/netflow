99c5865cee917d0e0d036ad3faa4c54e57de8a39
Steve ZARETTI <kossolax@gmail.com>
2022-09-27 15:04:20 +0200

    upgrade version, attempt to remove license
    #1

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { registerLicense } from '@syncfusion/ej2-base';
-registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkNhWX5ecn1RQ2dbWEI=');
-
-
 if (environment.production) {
   enableProdMode();
 }
